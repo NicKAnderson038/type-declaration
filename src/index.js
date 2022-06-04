c144fa87863ee8f59e2ec0f80a4750f9ee4a9a4f
NicKAnderson038 <nick7anderson@protonmail.com>
2022-06-04 11:30:45 -0700

    🚀 more test functions

--- src/index.js
+++ src/index.js
@@ -5,4 +5,10 @@ const object = {
 console.log("The organisation is ".concat(object.name, 
     " and it was founded in ").concat(object.founded));
 
+function funky({ one ='1', two = 2}) {
+    return one + 2
+}
+
+const array = ['one', 'two', 'three']
+
 export {};
