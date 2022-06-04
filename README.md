0672220edf2a460e0b732843a539afc2ad9f4751
Nick Anderson <nick7anderson@protonmail.com>
2022-06-04 11:12:57 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# Type Check
+
+1. Run `npm i`
+2. Run `npm run gen-types` to generate a index.d.ts file
+3. To place types in seperate location run `npm run gen-types-specific-folder`
---
+++ src/index.js
@@ -0,0 +1,8 @@
+const object = {
+    name: "GeeksforGeeks",
+    founded: 2009
+};
+console.log("The organisation is ".concat(object.name, 
+    " and it was founded in ").concat(object.founded));
+
+export {};
