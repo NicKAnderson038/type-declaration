cc37e28d9d7c791573f0146a55221a16029cc100
NicKAnderson038 <nick7anderson@protonmail.com>
2022-06-04 12:00:47 -0700

    📝 notes

--- src/index.js
+++ src/index.js
@@ -5,10 +5,28 @@ const object = {
 console.log("The organisation is ".concat(object.name, 
     " and it was founded in ").concat(object.founded));
 
-function funky({ one ='1', two = 2}) {
+function funky({ one = '1', two = 2}) {
     return one + 2
 }
 
 const array = ['one', 'two', 'three']
 
-// export {};
+/**
+ * Discovery notes
+ * export {} is required for one variable to prevent TS linting error.
+ * More than one variable must be exported to prevent TS linting error.
+ * 
+ * Question
+ * 1. Do we want all functions to be exportable at a top level?
+ * 2. Not importing the d.TS file into the top. Are these Types being set globally? Will naming conflicts be an issue?
+ * 
+ * Thoughts
+ * Proving useful for a single JS file.
+ */
+
+// export {}; // necessary for 1 variable in a file.
+export {
+    funky,
+    object,
+    array
+}
